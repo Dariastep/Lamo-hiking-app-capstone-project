8cddb4c07e7e97be650cc0d65c2fec33b57cd317
Daria Stepanova <d.stepanova1993@gmail.com>
2023-07-12 17:09:25 +0200

    fixed the test

--- components/Avatar/avatar.test.js
+++ components/Avatar/avatar.test.js
@@ -1,11 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import Avatar from "./";
 
-
 test("alt contains correct value", () => {
   render(<Avatar />);
   const testImage = screen.getByAltText("Default avatar");
-
 
   expect(testImage).toBeInTheDocument();
 });
