661118c18c131a052d70d1fa7497666b6713cceb
Daria Stepanova <d.stepanova1993@gmail.com>
2023-07-15 10:18:15 +0200

    nextAuth setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,29 @@
 import NavigationBar from "../components/NavigationBar/index.js";
 import GlobalStyle from "../styles";
 import { SWRConfig } from "swr";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <SWRConfig
-      value={{
-        fetcher: async (...args) => {
-          const response = await fetch(...args);
-          if (!response.ok) {
-            throw new Error(`Request with ${JSON.stringify(args)} failed.`);
-          }
-          return await response.json();
-        },
-      }}
-    >
-      <GlobalStyle />
-      <Component {...pageProps} />
-      <NavigationBar />
-    </SWRConfig>
+    <SessionProvider session={session}>
+      <SWRConfig
+        value={{
+          fetcher: async (...args) => {
+            const response = await fetch(...args);
+            if (!response.ok) {
+              throw new Error(`Request with ${JSON.stringify(args)} failed.`);
+            }
+            return await response.json();
+          },
+        }}
+      >
+        <GlobalStyle />
+        <Component {...pageProps} />
+        <NavigationBar />
+      </SWRConfig>
+    </SessionProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,34 @@ import Logo from "../components/Logo/Logo.js";
 import useSWR from "swr";
 import { toggleFavorite } from "../utils/toggleFavorite.js";
 import Loader from "../components/Loader/";
-
-
+import { useSession, signIn, signOut } from "next-auth/react";
+import CommonButton from "../components/CommonButton/index.js";
 
 export default function HomePage() {
+  const { data: session } = useSession();
   const { data: routesData, error } = useSWR("/api/routes", {
     fallbackData: [],
   });
   if (!routesData) {
-    return <Loader />// Render the loader component while data is being fetched
+    return <Loader />; // Render the loader component while data is being fetched
   } else if (error) {
     return <div>Error: {error.message}</div>;
   }
   return (
     <>
-    
       <Header title={<Logo />} />
       <MainSection>
+        <StyledSection>
+          {session ? ( // check if we have session data (= user is already signed in => display a logout button)
+            <>
+              <CommonButton onClick={signOut}>Logout</CommonButton>
+              <p>Signed in as {session.user.email}</p>
+            </>
+          ) : (
+            // no session data available yet, display a login button
+            <CommonButton onClick={() => signIn("github")}>Login</CommonButton>
+          )}
+        </StyledSection>
         <RouteList routesData={routesData} toggleFavorite={toggleFavorite} />
       </MainSection>
     </>
@@ -32,4 +43,10 @@ const MainSection = styled.div`
   margin-top: 6rem;
   display: flex;
   flex-direction: column;
+`;
+const StyledSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
 `;
