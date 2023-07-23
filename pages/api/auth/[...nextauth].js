4d7c610bae047f8e5f2fa22dcf56b340ca70b5c2
Daria Stepanova <d.stepanova1993@gmail.com>
2023-07-23 19:41:37 +0200

    added the option to sign in with credentials in production env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,32 +2,65 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-const providers = [
-  GithubProvider({
-    clientId: process.env.GITHUB_ID,
-    clientSecret: process.env.GITHUB_SECRET,
-  }),
-
-  CredentialsProvider({
-    name: "Credentials",
-    credentials: {
-      username: { label: "Username", type: "text", placeholder: "username" },
-      password: { label: "Password", type: "password" },
-    },
-    async authorize(credentials) {
-      // Implement your custom logic for validating credentials and signing in here
-      if (credentials.username === "lamo" && credentials.password === "lamo") {
-        return {
-          id: "1",
-          name: "Daria",
-          email: "d.stepanova1993@gmail.com",
-        };
-      } else {
-        return null;
-      }
-    },
-  }),
-];
+const providers = [];
+// Check if we are running on preview
+if (process.env.VERCEL_ENV === "preview") {
+  providers.push(
+    // Create a credentials provider with dummy data, describing input fields:
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "username" },
+        password: { label: "Password", type: "password" },
+      },
+      // and adding a fake authorization with static username and password:
+      async authorize(credentials) {
+        if (
+          credentials.username === "Daria" &&
+          credentials.password === "fishbone"
+        ) {
+          return {
+            id: "1",
+            name: "Daria",
+            email: "d.stepanova1993@gmail.com",
+          };
+        } else {
+          return null;
+        }
+      },
+    })
+  );
+} else {
+  // If not on preview, we use the GithubProvider as before
+  providers.push(
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "username" },
+        password: { label: "Password", type: "password" },
+      },
+      // and adding a fake authorization with static username and password:
+      async authorize(credentials) {
+        if (
+          credentials.username === "Daria" &&
+          credentials.password === "fishbone"
+        ) {
+          return {
+            id: "1",
+            name: "Daria",
+            email: "d.stepanova1993@gmail.com",
+          };
+        } else {
+          return null;
+        }
+      },
+    })
+  );
+}
 
 export const authOptions = {
   providers,
